c84777fbe4c37a75aa7d77c779a9b862c1488cc5
PEMILIO-PUTEQ <2021171021@uteq.edu.mx>
2026-03-26 14:30:43 -0600

    prometheus

--- app.js
+++ app.js
@@ -2,19 +2,44 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-
+var client = require("prom-client"); // 👈 agregar
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var itemsRouter = require("./routes/items");
 
 var app = express();
 
+// 👈 Métricas de Prometheus
+client.collectDefaultMetrics();
+
+const httpRequestCounter = new client.Counter({
+  name: 'http_requests_total',
+  help: 'Total de peticiones HTTP procesadas',
+  labelNames: ['metodo', 'ruta', 'estado_http'],
+});
+
+const activeUsersGauge = new client.Gauge({
+  name: 'active_users_current',
+  help: 'Número actual de usuarios activos simulados'
+});
+
+// 👈 Middleware para contar peticiones
+app.use((req, res, next) => {
+  res.on('finish', () => {
+    httpRequestCounter.inc({
+      metodo: req.method,
+      ruta: req.path,
+      estado_http: res.statusCode.toString(),
+    });
+  });
+  next();
+});
+
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/items", itemsRouter);
@@ -22,4 +47,16 @@ app.use("/items", itemsRouter);
 app.get('/health', (req, res) => {
   res.status(200).json({ status: 'OK' });
 });
+
+// 👈 Endpoint de métricas para Prometheus
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', client.register.contentType);
+  res.send(await client.register.metrics());
+});
+
+// 👈 Simular usuarios activos (sube y baja aleatoriamente)
+setInterval(() => {
+  activeUsersGauge.set(Math.floor(Math.random() * 100));
+}, 5000);
+
 module.exports = app;
