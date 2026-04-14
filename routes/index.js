3a2600335fcac10a192fc168b75728816e63eb73
PEmilio <palomillo.flores@gmail.com>
2026-04-13 19:47:00 -0600

    Add health check endpoint to routes

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,10 @@
 
+var express = require("express");
+var router = express.Router();
+
+router.get("/health", (req, res) => {
+  res.status(200).json({ status: "OK", uptime: process.uptime() });
+});
+
+module.exports = router;
+
