3f9fb39decef23dc6addbee2949243cb3d004e3d
PEmilio <palomillo.flores@gmail.com>
2026-04-13 18:25:23 -0600

    Delete index.js route file
    Remove the express router and its associated route.

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,1 @@
-var express = require("express");
-var router = express.Router();
 
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express - Deploy Automático' });
-});
-
-module.exports = router;
